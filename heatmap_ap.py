16fc472e5b9b9e29837694ed8d768156cb68e7d5
Md. Mirajul Islam <0419052071@grad.cse.buet.ac.bd>
2020-09-03 02:51:53 +0600

    Second update

--- heatmap_ap.py
+++ heatmap_ap.py
@@ -10,9 +10,13 @@ plt.rcParams['xtick.labeltop'] = True
 
 sns.set(font_scale=0.5)
 
-df = pd.read_excel("heatmap_ap_south.xlsx", index_col=0, header=0)
+df = pd.read_excel("heatmap_ap_north.xlsx", index_col=0, header=0)
 fig, ax = plt.subplots(figsize=(8,5.9))
-ax1 = sns.heatmap(df,  vmin=0, vmax=15, ax=ax, cbar=False, cmap="YlGnBu", square=True)
+#flatui = ["#e74c3c", "#3498db", "#2ecc71"]
+#sns.set_palette(flatui)
+#cmap=sns.color_palette()
+cmap=sns.color_palette("OrRd", 15)
+ax1 = sns.heatmap(df, vmin=0, vmax=15, ax=ax, cbar=False, cmap=cmap, square=True)
 plt.tight_layout()
 plt.savefig("test.svg",bbox_inches='tight')
 plt.show()
--- heatmap_ca.py
+++ heatmap_ca.py
@@ -8,12 +8,16 @@ plt.rcParams['xtick.labelbottom'] = False
 plt.rcParams['xtick.top'] = False
 plt.rcParams['xtick.labeltop'] = True
 
-sns.set(font_scale=0.5)
+sns.set(font_scale=0.8)
 
 df = pd.read_excel("heatmap_ca_south.xlsx", index_col=0, header=0)
 
 fig, ax = plt.subplots(figsize=(12, 7))
-ax1 = sns.heatmap(df,  vmin=0, vmax=20, ax=ax, cbar=False, cmap="YlGnBu", square=True)
+#flatui = ["#e74c3c", "#3498db", "#2ecc71"]
+#flatui = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71"]
+#cmap=sns.light_palette("red", as_cmap=True)
+#cmap=sns.color_palette("flatui")
+ax1 = sns.heatmap(df,  vmin=0, vmax=20, ax=ax, cbar=False, cmap="OrRd", square=True)
 plt.tight_layout()
 plt.savefig("test.svg",bbox_inches='tight')
 plt.show()
